5860755bffc666f3ae22a18a3cfcdab2dda83050
John Bellone <jbellone@bloomberg.net>
2015-09-02 09:56:44 -0400

    Updates files after running rubocop.

--- recipes/cpu.rb
+++ recipes/cpu.rb
@@ -1,25 +1,13 @@
 #
-# Cookbook Name:: collectd_plugins
-# Recipe:: cpu
+# Cookbook: collectd_plugins
+# License: Apache 2.0
 #
 # Copyright 2010, Atari, Inc
+# Copyright 2015, Bloomberg Finance L.P.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+include_recipe 'collectd::default'
 
-include_recipe "collectd"
-
-collectd_plugin "cpu" do
+collectd_plugin 'cpu' do
   user node['collectd']['service_user']
   group node['collectd']['service_group']
   options node['collectd-plugins']['cpu']['options'] unless node['collectd-plugins']['cpu'].nil?
