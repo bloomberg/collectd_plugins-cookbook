24734b9e0458f8a0c496d8376a7bc238d0310cba
John Bellone <jbellone@bloomberg.net>
2015-09-25 10:34:45 -0400

    Bumps version of the cookbook.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'noah@coderanger.net'
 license 'Apache 2.0'
 description 'Application cookbook which configures collectd plugins.'
 long_description 'Application cookbook which configures collectd plugins.'
-version '2.0.0'
+version '2.0.1'
 
 supports 'ubuntu', '>= 10.04'
 supports 'centos', '>= 5.8'
