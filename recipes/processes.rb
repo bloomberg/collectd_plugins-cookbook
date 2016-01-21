ff67d79407e23050b1120d9e740cb41e6ff5b68f
jquinn1 <jquinn47@bloomberg.net>
2016-01-21 09:15:50 -0500

    Adding processes plugin recipe

---
+++ recipes/processes.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook: collectd_plugins
+# License: Apache 2.0
+#
+# Copyright 2015, Bloomberg Finance L.P.
+#
+include_recipe 'collectd::default'
+
+collectd_plugin 'processes' do
+  user node['collectd']['service_user']
+  group node['collectd']['service_group']
+  directory node['collectd']['service']['config_directory']
+  options node['collectd-plugins']['processes']
+  notifies :restart, "collectd_service[#{node['collectd']['service_name']}]", :delayed
+end
